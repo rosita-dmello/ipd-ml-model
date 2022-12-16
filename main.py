5372a3f76150232a890b499cb210eba46fa59cfa
Rosita <esorthe7@gmail.com>
2022-12-16 22:14:25 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import pickle
+from sklearn.svm import SVC
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+from pydantic import BaseModel
+
+model = pickle.load(open('model.sav', 'rb'))
+print(type(model))
+from fastapi import FastAPI, Path
+app = FastAPI()
+@app.get("/")
+def home():
+    return {
+        "test": "testing"
+    }
+
+class Item(BaseModel):
+    language: float
+    memory: float
+    speed: float
+    visual:  float
+    audio: float
+    survey: float
+
+def get_result(lang_vocab, memory, speed, visual, audio, survey):
+    #2D numpy array created with the values input by the user.
+    array = np.array([[lang_vocab, memory, speed, visual, audio, survey]])
+    #The output given by model is converted into an int and stored in label.
+    label = int(model.predict(array))
+    #Giving final output to user depending upon the model prediction.
+    if(label == 0):
+        output = "There is a high chance of the applicant to have dyslexia."
+    elif(label == 1):
+        output = "There is a moderate chance of the applicant to have dyslexia."
+    else:
+        output = "There is a low chance of the applicant to have dyslexia."
+    return output
+
+@app.post("/result")
+def result(scores:Item):
+    return get_result(scores.language, scores.memory, scores.speed, scores.visual, scores.audio, scores.survey)
+    
+
+
